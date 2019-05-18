996850ce99c3dfb8d07f07eefb8595d389729535
Harishankar <harishankar714@gmail.com>
2019-05-18 19:14:07 +0530

    slight modification in login act

--- app/src/main/java/com/hvdcreations/tagit/LoginActivity.java
+++ app/src/main/java/com/hvdcreations/tagit/LoginActivity.java
@@ -1,24 +1,35 @@
 package com.hvdcreations.tagit;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.PhoneAuthProvider;
-import com.google.firebase.database.FirebaseDatabase;
+import com.rilixtech.Country;
+import com.rilixtech.CountryCodePicker;
 
-import rezwan.pstu.cse12.view.CircularMorphLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView TxtLogin;
+    CountryCodePicker ccp;
+    EditText ETPhone;
+    ProgressBar progressBar;
+
+    ConstraintLayout loginscreen, verifyscreen;
+
+    String code = String.valueOf(91), phone = String.valueOf(0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +38,57 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        TxtLogin = findViewById(R.id.tv_login);
+        ccp = findViewById(R.id.countryCodePicker);
+        ETPhone = findViewById(R.id.et_phone);
+
+        loginscreen = findViewById(R.id.layout_login);
+        verifyscreen = findViewById(R.id.layout_verify);
+        progressBar = findViewById(R.id.progressBar);
+
+        ccp.registerPhoneNumberTextView(ETPhone);
+
+        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected(Country selectedCountry) {
+                code = selectedCountry.getPhoneCode();
+            }
+        });
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                phone = ETPhone.getText().toString();
+                if (phone == null) {
+                    Snackbar.make(view, "Enter your Phone Number", Snackbar.LENGTH_SHORT).show();
+                } else {
+                Toast.makeText(getBaseContext(), "+" + code + phone, Toast.LENGTH_SHORT).show();
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.INVISIBLE);
+                        loginscreen.setVisibility(View.INVISIBLE);
+                        verifyscreen.setVisibility(View.VISIBLE);
+                    }
+
+                }, 2000);
+            }
+
+
             }
         });
+    }
+
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 
 }
