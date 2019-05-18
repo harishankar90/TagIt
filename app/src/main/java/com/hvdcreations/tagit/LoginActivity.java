6934b9431f8ee5bbf73b0d0f56b756b7dfb726a2
Harishankar <harishankar714@gmail.com>
2019-05-18 20:40:32 +0530

    slight modification in login act

--- app/src/main/java/com/hvdcreations/tagit/LoginActivity.java
+++ app/src/main/java/com/hvdcreations/tagit/LoginActivity.java
@@ -1,13 +1,14 @@
 package com.hvdcreations.tagit;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -16,6 +17,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.goodiebag.pinview.Pinview;
 import com.rilixtech.Country;
 import com.rilixtech.CountryCodePicker;
 
@@ -26,8 +28,10 @@ public class LoginActivity extends AppCompatActivity {
     CountryCodePicker ccp;
     EditText ETPhone;
     ProgressBar progressBar;
+    FloatingActionButton fabLogin, fabVerify;
 
     ConstraintLayout loginscreen, verifyscreen;
+    Pinview pinview;
 
     String code = String.valueOf(91), phone = String.valueOf(0);
 
@@ -46,7 +50,9 @@ public class LoginActivity extends AppCompatActivity {
         verifyscreen = findViewById(R.id.layout_verify);
         progressBar = findViewById(R.id.progressBar);
 
-        ccp.registerPhoneNumberTextView(ETPhone);
+        pinview = findViewById(R.id.OTPpincode);
+
+        //ccp.registerPhoneNumberTextView(ETPhone);
 
         ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
             @Override
@@ -55,32 +61,52 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fabLogin = findViewById(R.id.fab);
+        fabVerify = findViewById(R.id.fab_verify);
+
+        fabLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                phone = ETPhone.getText().toString();
-                if (phone == null) {
-                    Snackbar.make(view, "Enter your Phone Number", Snackbar.LENGTH_SHORT).show();
-                } else {
-                Toast.makeText(getBaseContext(), "+" + code + phone, Toast.LENGTH_SHORT).show();
 
-                progressBar.setVisibility(View.VISIBLE);
+                phone = ETPhone.getText().toString();
+                if (phone.isEmpty()) {
+                    Snackbar.make(view, "Pls. Enter your  Phone no.", Snackbar.LENGTH_LONG).show();
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
 
                 new Handler().postDelayed(new Runnable() {
+
+                    @SuppressLint("RestrictedApi")
                     @Override
                     public void run() {
                         progressBar.setVisibility(View.INVISIBLE);
                         loginscreen.setVisibility(View.INVISIBLE);
                         verifyscreen.setVisibility(View.VISIBLE);
+                        fabLogin.setVisibility(View.INVISIBLE);
+                        fabVerify.setVisibility(View.VISIBLE);
                     }
 
                 }, 2000);
-            }
 
+                    Toast.makeText(getBaseContext(), "+" + code + phone, Toast.LENGTH_SHORT).show();
+
+            }
 
             }
         });
+
+        fabVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pin = pinview.getValue();
+                Toast.makeText(LoginActivity.this, pin, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 
     private void closeKeyboard() {
