4dcb13f8e3629f857edaccffa4633d5d5df800db
Harishankar <harishankar714@gmail.com>
2019-05-20 19:35:04 +0530

    finished the phone auth using firebase, need to work on data structure

--- app/src/main/java/com/hvdcreations/tagit/LoginActivity.java
+++ app/src/main/java/com/hvdcreations/tagit/LoginActivity.java
@@ -1,10 +1,17 @@
 package com.hvdcreations.tagit;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.arch.core.executor.TaskExecutor;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,11 +25,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.goodiebag.pinview.Pinview;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.rilixtech.Country;
 import com.rilixtech.CountryCodePicker;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ConnectivityManager connectivityManager;
+
+    private FirebaseAuth mAuth;
 
     TextView TxtLogin;
     CountryCodePicker ccp;
@@ -35,12 +56,18 @@ public class LoginActivity extends AppCompatActivity {
 
     String code = String.valueOf(91), phone = String.valueOf(0);
 
+    String verificationID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        mAuth = FirebaseAuth.getInstance();
 
         TxtLogin = findViewById(R.id.tv_login);
         ccp = findViewById(R.id.countryCodePicker);
@@ -64,13 +91,28 @@ public class LoginActivity extends AppCompatActivity {
         fabLogin = findViewById(R.id.fab);
         fabVerify = findViewById(R.id.fab_verify);
 
+        NetworkInfo networkInfo =   connectivityManager.getActiveNetworkInfo();
+        if (networkInfo==null){
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("You SmartPhone is useless, if you are NOT connected to the INTERNET !");
+                    alertDialogBuilder.setPositiveButton("Settings",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface arg0, int arg1) {
+                                    Intent intent = new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+                                    startActivity(intent);                                }
+                            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+
         fabLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 phone = ETPhone.getText().toString();
                 if (phone.isEmpty()) {
-                    Snackbar.make(view, "Pls. Enter your  Phone no.", Snackbar.LENGTH_LONG).show();
+                    Snackbar.make(view, "Nice Try, Enter your Phone no.", Snackbar.LENGTH_LONG).show();
                 } else {
                     progressBar.setVisibility(View.VISIBLE);
 
@@ -88,7 +130,8 @@ public class LoginActivity extends AppCompatActivity {
 
                 }, 2000);
 
-                    Toast.makeText(getBaseContext(), "+" + code + phone, Toast.LENGTH_SHORT).show();
+                String number = "+" + code + phone;
+                sendVerificationCode(number);
 
             }
 
@@ -99,15 +142,66 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String pin = pinview.getValue();
-                Toast.makeText(LoginActivity.this, pin, Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
+                if (pin.isEmpty() || pin.length()<6){
+                    Snackbar.make(v, "Nice Try, Wait for OTP !", Snackbar.LENGTH_LONG).show();
+                }
+                verifyCode(code);
 
             }
         });
 
     }
+
+    private void verifyCode(String code){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationID,code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private void sendVerificationCode(String number) {
+        Toast.makeText(this, "Your number is "+number, Toast.LENGTH_SHORT).show();
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(number,60, TimeUnit.SECONDS, TaskExecutors.MAIN_THREAD,mCallBack);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationID = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if (code!=null){
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(LoginActivity.this);
+            alertDialogBuilder.setMessage("Verification Failed !");
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+    };
 
     private void closeKeyboard() {
         View view = this.getCurrentFocus();
