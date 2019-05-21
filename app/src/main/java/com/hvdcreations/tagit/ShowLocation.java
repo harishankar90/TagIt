272d56df59f8b2757dde1a7b60ec5c7b1d0126ac
Harishankar <harishankar714@gmail.com>
2019-05-21 16:56:50 +0530

    working on retrieveing data

--- app/src/main/java/com/hvdcreations/tagit/ShowLocation.java
+++ app/src/main/java/com/hvdcreations/tagit/ShowLocation.java
@@ -1,13 +1,52 @@
 package com.hvdcreations.tagit;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
 
 public class ShowLocation extends AppCompatActivity {
+
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("users");
+
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_location);
+
+        txt = findViewById(R.id.textView);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                try{
+                    String value = dataSnapshot.getValue(String.class);
+                    Toast.makeText(ShowLocation.this, value, Toast.LENGTH_SHORT).show();
+                }catch (Exception e){
+                    Toast.makeText(ShowLocation.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ShowLocation.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 }
